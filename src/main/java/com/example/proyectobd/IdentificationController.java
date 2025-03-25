bc1d056152829635595efad8ce75cd7a4daffa02
fbustios <francobustios07@gmail.com>
2025-03-24 23:38:10 -0600

    24 de marzo latenight

--- src/main/java/com/example/proyectobd/IdentificationController.java
+++ src/main/java/com/example/proyectobd/IdentificationController.java
@@ -1,4 +1,14 @@
 package com.example.proyectobd;
 
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class IdentificationController {
+
+    @PostMapping("home/helpIdentify")
+    public void addIdentification(@RequestParam String algo){
+
+    }
 }
--- src/main/java/com/example/proyectobd/ObservationController.java
+++ src/main/java/com/example/proyectobd/ObservationController.java
@@ -1,4 +1,16 @@
 package com.example.proyectobd;
 
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
 public class ObservationController {
+
+
+    @PostMapping("home/reportObservation")
+    public void addObservation(@RequestParam("nose") String name){
+
+    }
 }
